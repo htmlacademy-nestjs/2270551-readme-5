ce4d98c7d0e9feee91c9df9bc9661c1260bdab3a
Greg <grtop@yandex.ru>
2023-12-10 21:47:08 +0300

    Добавит сущность BlogUserEntity

--- project/libs/shared/app-types/src/lib/user.interface.ts
+++ project/libs/shared/app-types/src/lib/user.interface.ts
@@ -1,9 +1,12 @@
+import { UserStatus } from "./user-status.enum";
+
 export interface User {
   _id?: string;
   email: string;
   firstname: string;
   lastname: string;
   avatar?: string;
+  status: UserStatus;
   postsCount?:number;
   subscribersCount?:number;
 }
