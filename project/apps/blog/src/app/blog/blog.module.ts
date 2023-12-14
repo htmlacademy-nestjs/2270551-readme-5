431a582a694aeacad85c104b0818b7ea69ee8fb3
Greg <grtop@yandex.ru>
2023-12-15 00:29:22 +0300

    сервис для блога

---
+++ project/apps/blog/src/app/blog/blog.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { BlogController } from './blog.controller';
+import { BlogService } from './blog.service';
+import { BlogRepository } from './blog.repository';
+import { VideoBlogRepository } from './video-content/video-blog.repository';
+import { BaseBlogContentService } from './base-blog/blog.service';
+import { TextBlogRepository } from './text-content/text-blog.repository';
+import { LinkBlogRepository } from './link-content/link-blog.repository';
+import { PhotoBlogRepository } from './photo-content/photo-blog.repository';
+import { QuoteBlogRepository } from './quote-content/quote-blog.repository';
+
+@Module({
+  imports: [],
+  controllers: [BlogController],
+  providers: [BlogService, BlogRepository, VideoBlogRepository, TextBlogRepository, LinkBlogRepository, PhotoBlogRepository, QuoteBlogRepository, BaseBlogContentService],
+  exports: [BlogRepository]
+})
+export class BlogModule {}
