81a27450d407cde0f2a67c3d57f1193e44d059de
Greg <grtop@yandex.ru>
2023-12-29 23:47:02 +0300

    создал библиотеку config-account.module

--- project/libs/shared/config/account/src/lib/config-account.modules.ts
+++ project/libs/shared/config/account/src/lib/config-account.modules.ts
@@ -1,3 +1,17 @@
-export function sharedConfigAccount(): string {
-  return 'shared-config-account';
-}
+import { Module } from '@nestjs/common';
+ import { ConfigModule } from '@nestjs/config';
+
+ const ENV_USERS_FILE_PATH = 'apps/account/account.env';
+
+ @Module({
+   imports: [
+     ConfigModule.forRoot({
+       isGlobal: true,
+       cache: true,
+       // TODO: Передать список конфигураций для загрузки
+       load: [],
+       envFilePath: ENV_USERS_FILE_PATH
+     }),
+   ]
+ })
+ export class ConfigAccountModule {}
