f195e130ee95fcd443d101bd22cdfab2983cd11f
Greg <grtop@yandex.ru>
2024-01-27 21:52:33 +0300

    добавил кол-во страниц и постов на странице

--- project/apps/blog/src/app/blog/constants.ts
+++ project/apps/blog/src/app/blog/constants.ts
@@ -1,8 +1,13 @@
+import { SortDirection, SortType } from '@project/libs/shared/app-types';
+import { BlogType } from '@project/libs/shared/app-types';
+
 export const NOT_CREATE_BLOG_CONTENT = 'Blog content can not created';
 export const NOT_FOUND_BLOG = 'Blog not found';
 export const NOT_FOUND_BLOG_CONTENT = 'Blog content not found';
-
-import { BlogType } from '@project/libs/shared/app-types';
+export const DEFAULT_PAGE_COUNT = 1;
+export const DEFAULT_POST_COUNT_LIMIT = 25;
+export const DEFAULT_SORT_DIRECTION = SortDirection.Desc;
+export const DEFAULT_SORT = SortType.Date;
 
 export const BLOG_REPOSITORY_MAP = {
   [BlogType.Video]: 'videoBlogRepository',
