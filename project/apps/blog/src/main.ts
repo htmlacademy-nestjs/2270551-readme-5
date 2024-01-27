e5b3323950d58898ea6a942dffc94e6e266a0dce
Greg <grtop@yandex.ru>
2024-01-28 02:17:23 +0300

    Подключит ValidationPipe глобально

--- project/apps/blog/src/main.ts
+++ project/apps/blog/src/main.ts
@@ -3,11 +3,12 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
    SwaggerModule.setup('spec', app, document);
+   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
