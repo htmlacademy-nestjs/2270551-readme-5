471dcbc51f8e541cded0c56c35ec469ec84f6ae2
Greg <grtop@yandex.ru>
2024-01-28 04:08:04 +0300

    правка

--- project/libs/shared/app-types/src/lib/token-payload.interface.ts
+++ project/libs/shared/app-types/src/lib/token-payload.interface.ts
@@ -3,7 +3,7 @@ import { UserStatus } from './user-status.enum';
  export interface TokenPayload {
    sub: string;
    email: string;
-   staus: UserStatus;
+   status: UserStatus;
    lastname: string;
    firstname: string;
  }
