50e8a2e936e22d123509af3761336dfe8f3bf61d
Greg <grtop@yandex.ru>
2024-01-28 04:03:35 +0300

    Добавит метод createUserToken в AuthenticationService

--- project/apps/account/src/app/authentication/authentication.service.ts
+++ project/apps/account/src/app/authentication/authentication.service.ts
@@ -1,8 +1,13 @@
-import { ConflictException, Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Logger, ConflictException, HttpException,
+  HttpStatus, Injectable, NotFoundException,
+  UnauthorizedException
+} from '@nestjs/common';
 import dayjs from 'dayjs';
+import { JwtService } from '@nestjs/jwt';
+import { Token, TokenPayload, User, UserStatus } from '@project/libs/shared/app-types';
 import { ConfigType } from '@nestjs/config';
 import { dbConfig } from '@project/shared/config/account';
-import { UserStatus } from '@project/libs/shared/app-types';
 import { BlogUserRepository } from '../blog-user/blog-user.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constant';
@@ -12,63 +17,64 @@ import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthenticationService {
+  private readonly logger = new Logger(AuthenticationService.name);
   constructor(
     private readonly blogUserRepository: BlogUserRepository,
+    private readonly jwtService: JwtService,
+    ) {}
 
-    @Inject(dbConfig.KEY)
-    private readonly databaseConfig: ConfigType<typeof dbConfig>,
-  ) {
-     // Извлекаем настройки из конфигурации
-     console.log(databaseConfig.host);
-     console.log(databaseConfig.user);
+    public async register(dto: CreateUserDto) {
+      const {email, firstname, lastname, password} = dto;
+      const blogUser = {
+        email, firstname, lastname, status: UserStatus.User,
+        avatar: '', dateOfBirth: dayjs(dateBirth).toDate(),
+        passwordHash: ''
+      };
+      const existUser = await this.blogUserRepository
+      .findByEmail(email);
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+    const userEntity = await new BlogUserEntity(blogUser)
+      .setPassword(password)
+    return this.blogUserRepository
+      .save(userEntity);
   }
-
-  public async register(dto: CreateUserDto) {
-    const {email, firstname, lastname, password } = dto;
-
-    const blogUser = {
-      email, firstname, lastname, status: UserStatus.User,
-      avatar: '',
-      passwordHash: ''
-    };
-
-    const existUser = await this.blogUserRepository
-    .findByEmail(email);
-
-  if (existUser) {
-    throw new ConflictException(AUTH_USER_EXISTS);
+  public async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+    if (!await existUser.comparePassword(password)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+    return existUser;
   }
+  public async getUser(id: string) {
+    const existUser = await this.blogUserRepository.findById(id);
+    if (! existUser) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
 
-  const userEntity = await new BlogUserEntity(blogUser)
-    .setPassword(password)
+     return existUser;
+   }
 
-  return this.blogUserRepository
-    .save(userEntity);
-}
+   public async createUserToken(user: User): Promise<Token> {
+     const payload: TokenPayload = {
+       sub: user.id,
+       email: user.email,
+       status: user.status,
+       lastname: user.lastname,
+       firstname: user.firstname,
+     };
 
-public async verifyUser(dto: LoginUserDto) {
-  const {email, password} = dto;
-  const existUser = await this.blogUserRepository.findByEmail(email);
-
-  if (!existUser) {
-    throw new NotFoundException(AUTH_USER_NOT_FOUND);
-  }
-
-  if (!await existUser.comparePassword(password)) {
-    throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
-  }
-
-  return existUser;
-}
-
-public async getUser(id: string) {
-  const existUser = await this.blogUserRepository.findById(id);
-
-  if (! existUser) {
-    throw new NotFoundException(`User with id ${id} not found`);
-  }
-
-  return existUser;
-}
-
-}
+     try {
+       const accessToken = await this.jwtService.signAsync(payload);
+       return { accessToken };
+     } catch (error) {
+       this.logger.error('[Token generation error]: ' + error.message);
+       throw new HttpException('Ошибка при создании токена.', HttpStatus.INTERNAL_SERVER_ERROR);
+     }
+   }
+ }
