276a2d8575f4534b852b5449ba8d4c0c97a69524
Greg <grtop@yandex.ru>
2024-01-30 00:12:18 +0300

    подготовил библиотеку blog

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from './blog/blog.module';
 import { LikeModule } from './likes/like.module';
-
+import {BlogConfigModule} from '@project/libs/shared/config/blog';
 
 @Module({
-  imports: [BlogModule, LikeModule],
+  imports: [BlogModule, LikeModule,BlogConfigModule],
   controllers: [],
   providers: [],
 })
