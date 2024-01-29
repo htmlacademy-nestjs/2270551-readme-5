673d1e7d5c07403081ed7eb151a32a1375e91f4d
Greg <grtop@yandex.ru>
2024-01-30 00:13:02 +0300

    библиотека blog

--- project/libs/shared/blog/models/src/lib/libs/shared/blog/models.spec.ts
+++
@@ -1,7 +0,0 @@
-import { libsSharedBlogModels } from './libs/shared/blog/models';
-
-describe('libsSharedBlogModels', () => {
-  it('should work', () => {
-    expect(libsSharedBlogModels()).toEqual('libs/shared/blog/models');
-  });
-});
