f6cdceac0efa70c1d3098bf26fc50a67d0b8cb9f
Greg <grtop@yandex.ru>
2024-01-29 20:56:26 +0300

    Добавит EmailSubscriberController

--- project/apps/notify/src/app/email-subscriber/email-subscriber.module.ts
+++ project/apps/notify/src/app/email-subscriber/email-subscriber.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 import { EmailSubscriberModel, EmailSubscriberSchema } from './email-subscriber.model';
 import { EmailSubscriberService } from './email-subscriber.service';
 import { EmailSubscriberRepository } from './email-subscriber.repository';
+import { getRabbitMQOptions } from '@project/libs/shared/helpers';
+import { EmailSubscriberController } from './email-subscriber.controller';
 
  @Module({
    imports: [
      MongooseModule.forFeature([
        { name: EmailSubscriberModel.name, schema: EmailSubscriberSchema }
      ]),
+     RabbitMQModule.forRootAsync(
+      RabbitMQModule,
+      getRabbitMQOptions('application.rabbit')
+    ),
    ],
+   controllers: [
+    EmailSubscriberController,
+  ],
    providers: [
      EmailSubscriberService,
      EmailSubscriberRepository,
