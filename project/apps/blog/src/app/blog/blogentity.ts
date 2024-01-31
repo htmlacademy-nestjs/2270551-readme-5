77389d524abc9ae30cc5ca437294bec5bf8d5103
Greg <grtop@yandex.ru>
2024-02-01 01:06:38 +0300

    подготовка к защите

---
+++ project/apps/blog/src/app/blog/blogentity.ts
@@ -0,0 +1,60 @@
+import {Blog, BlogContent, BlogStatus, BlogType} from '@project/libs/shared/app-types'
+import { Category } from '@project/libs/shared/app/types';
+import { PrismaClientService } from '@project/libs/shared/blog/models';
+
+export class BlogEntity implements Blog {
+  public type: BlogType;
+  public id?: string;
+  public content: BlogContent;
+  public createdDate?: Date;
+  public postedDate?: Date;
+  public status?: BlogStatus;
+  public userId: string;
+  public tags?: string[];
+  public repost?: boolean;
+  public repostId?: string;
+  public repostUserId?: string;
+  public categoies: string[];
+
+  constructor(blog: Blog) {
+    this.populate(blog);
+  }
+  categories: Category[];
+
+
+  public toPOJO() {
+    return {
+      type: this.type,
+      id: this.id,
+      userId: this.userId,
+      createdDate: this.createdDate,
+      postedDate: this.postedDate,
+      repost: this.repost,
+      repostId: this.repostId,
+      repostUserId: this.repostUserId,
+      status: this.status,
+      categories: [],
+      tags: this.tags,
+      content: this.content
+    };
+  }
+
+  public populate(data: Blog): void {
+    this.type = data.type;
+    this.id = data.id;
+    this.userId = data.userId;
+    this.createdDate = data.createdDate;
+    this.postedDate = data.postedDate;
+    this.repost = data.repost;
+    this.repostId = data.repostId;
+    this.repostUserId = data.repostId;
+    this.status = data.status;
+    this.tags = data.tags;
+    this.content = data.content;
+  }
+
+  static fromObject(data: Blog, client: PrismaClientService, fromObject: any): BlogEntity {
+    return new BlogEntity(data);
+  }
+
+}
--- project/apps/blog/src/app/blog/utils.ts
+++ project/apps/blog/src/app/blog/utils.ts
@@ -1,12 +1,12 @@
-import { BlogType, SortDirection, SortType } from '@project/libs/shared/app-types';
- import { VideoBlogEntity } from './video-content/video-blog.entity';
- import { LinkBlogEntity } from './link-content/link-blog.entity';
- import { PhotoBlogEntity } from './photo-content/photo-blog.entity';
- import { QuoteBlogEntity } from './quote-content/quote-blog.entity';
- import { TextBlogEntity } from './text-content/text-blog.entity';
- import { Prisma } from '@prisma/client';
+import { BlogStatus,BlogType, SortDirection, SortType } from '@project/libs/shared/app-types';
+import { VideoBlogEntity } from './video-content/video-blog.entity';
+import { LinkBlogEntity } from './link-content/link-blog.entity';
+import { PhotoBlogEntity } from './photo-content/photo-blog.entity';
+import { QuoteBlogEntity } from './quote-content/quote-blog.entity';
+import { TextBlogEntity } from './text-content/text-blog.entity';
+import { Prisma } from '@prisma/client';
 
- export type UnionBlogEntity = VideoBlogEntity | TextBlogEntity | LinkBlogEntity | PhotoBlogEntity | QuoteBlogEntity;
+export type UnionBlogEntity = VideoBlogEntity | TextBlogEntity | LinkBlogEntity | PhotoBlogEntity | QuoteBlogEntity;
 
 export const isVideoBlogEntity = (type: BlogType, entity: UnionBlogEntity): entity is VideoBlogEntity => {
   return type === BlogType.Video;
@@ -30,15 +30,33 @@ export const isPhotoBlogEntity = (type: BlogType, entity: UnionBlogEntity): enti
  }
 
  export interface BlogFilter {
-   type?: BlogType
- }
+  type?: BlogType,
+  search?: string,
+  tag?: string,
+}
+
 
  export function blogFilter(filter: BlogFilter): Prisma.BlogWhereInput | undefined {
    if (!filter) {
      return undefined;
    }
 
-   let prismaFilter: Prisma.BlogWhereInput = {};
+   let prismaFilter: Prisma.BlogWhereInput = {
+    status: BlogStatus.Public
+  };
+
+  if (filter.tag) {
+    prismaFilter.tags = {
+      hasSome: [filter.tag]
+    };
+  }
+
+  if (filter.search) {
+    prismaFilter.OR = [
+      {videoBlog: {name: {contains: filter.search}}},
+      {textBlog: {name: {contains: filter.search}}},
+    ];
+  }
 
    if (filter.type) {
      prismaFilter = { type: filter.type };
@@ -48,20 +66,20 @@ export const isPhotoBlogEntity = (type: BlogType, entity: UnionBlogEntity): enti
  }
 
  export function blogSort(sortParams: BlogSorting): Prisma.BlogOrderByWithRelationInput | undefined {
-   if (!sortParams) {
-     return undefined;
-   }
-   const {sort, direction} = sortParams;
-   let prismaFilter: Prisma.BlogOrderByWithRelationInput = {};
-   if (sort === SortType.Date) {
-     prismaFilter = {[sort]: direction}
-   }
+  if (!sortParams) {
+    return undefined;
+  }
+  const {sort, direction} = sortParams;
+  let prismaFilter: Prisma.BlogOrderByWithRelationInput = {};
+  if (sort === SortType.Date) {
+    prismaFilter = {[sort]: direction}
+  }
 
-   if (sort === SortType.Likes || sort === SortType.Comments) {
-     prismaFilter = { [sort]: {
-       _count: direction
-     } };
-   }
+  if (sort === SortType.Likes || sort === SortType.Comments) {
+    prismaFilter = { [sort]: {
+      _count: direction
+    } };
+  }
 
-   return prismaFilter;
- }
+  return prismaFilter;
+}
