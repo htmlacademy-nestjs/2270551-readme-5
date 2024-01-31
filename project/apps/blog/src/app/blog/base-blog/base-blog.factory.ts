fbaf8ca86301aff76381940a44eaf53b3c6b6c78
Greg <grtop@yandex.ru>
2024-01-31 23:25:03 +0300

    исправление по Б.12

---
+++ project/apps/blog/src/app/blog/base-blog/base-blog.factory.ts
@@ -0,0 +1,23 @@
+import { BlogContent, BlogType, LinkBlogContent, PhotoBlogContent, QuoteBlogContent, TextBlogContent, VideoBlogContent } from '@project/libs/shared/app-types';
+import { VideoBlogEntity } from '../video-content/video-blog.entity';
+import { TextBlogEntity } from '../text-content/text-blog.entity';
+import { LinkBlogEntity } from '../link-content/link-blog.entity';
+import { PhotoBlogEntity } from '../photo-content/photo-blog.entity';
+import { QuoteBlogEntity } from '../quote-content/quote-blog.entity';
+
+export const baseBlogEntityFactory = (type: BlogType, content: BlogContent) => {
+  switch(type) {
+    case (BlogType.Video):
+      return new VideoBlogEntity(content as VideoBlogContent);
+      case (BlogType.Text):
+        return new TextBlogEntity(content as TextBlogContent);
+      case (BlogType.Link):
+        return new LinkBlogEntity(content as LinkBlogContent);
+      case (BlogType.Photo):
+        return new PhotoBlogEntity(content as PhotoBlogContent);
+      case (BlogType.Quote):
+        return new QuoteBlogEntity(content as QuoteBlogContent);
+      default:
+      throw new Error('Not implements blog type');
+  }
+}
--- project/apps/blog/src/app/blog/blog.entity.ts
+++ project/apps/blog/src/app/blog/blog.entity.ts
@@ -1,27 +1,22 @@
 import {Blog, BlogContent, BlogStatus, BlogType} from '@project/libs/shared/app-types'
-import { Category } from '@project/libs/shared/app/types';
-import { PrismaClientService } from '@project/libs/shared/blog/models';
 
 export class BlogEntity implements Blog {
-  static fromObject(client: PrismaClientService, fromObject: any) {
-    throw new Error('Method not implemented.');
-  }
   public type: BlogType;
-  public id: string;
-  public contentId: string;
-  public createdDate: Date;
-  public postedDate: Date;
-  public status: BlogStatus;
-  public userId: string;
-  public tags: string[];
-  public repost: boolean;
-  public repostId: string;
+  public id?: string;
   public content: BlogContent;
+  public createdDate?: Date;
+  public postedDate?: Date;
+  public status?: BlogStatus;
+  public userId: string;
+  public tags?: string[];
+  public repost?: boolean;
+  public repostId?: string;
+  public repostUserId?: string;
+  public categories: [];
 
   constructor(blog: Blog) {
     this.populate(blog);
   }
-  categories: Category[];
 
 
   public toPOJO() {
@@ -29,15 +24,15 @@ export class BlogEntity implements Blog {
       type: this.type,
       id: this.id,
       userId: this.userId,
-      contentId: this.contentId,
       createdDate: this.createdDate,
       postedDate: this.postedDate,
       repost: this.repost,
       repostId: this.repostId,
+      repostUserId: this.repostUserId,
       status: this.status,
       tags: this.tags,
-      categories: this.categories,
       content: this.content,
+      categories: []
     };
   }
 
@@ -45,19 +40,17 @@ export class BlogEntity implements Blog {
     this.type = data.type;
     this.id = data.id;
     this.userId = data.userId;
-    this.content = data.content;
     this.createdDate = data.createdDate;
     this.postedDate = data.postedDate;
     this.repost = data.repost;
     this.repostId = data.repostId;
+    this.repostUserId = data.repostId;
     this.status = data.status;
     this.tags = data.tags;
+    this.content = data.content;
   }
 
-  public repostBlog(data: {repostId: string}) {
-    this.repost = true;
-    this.createdDate = new Date();
-    this.postedDate = new Date();
-    this.repostId = data.repostId;
+  static fromObject(data: Blog): BlogEntity {
+    return new BlogEntity(data);
   }
 }
