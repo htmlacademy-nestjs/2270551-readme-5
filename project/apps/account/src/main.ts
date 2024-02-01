70fd3f20679f7805895a36c0b14b6326a12f7a47
Greg <grtop@yandex.ru>
2024-02-01 14:04:19 +0300

    убрал лишнее

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -7,7 +7,6 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
